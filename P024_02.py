ff1c2684cee61660534017aa8142003f755ee86c
Daniel Suh <62726295+dsuh02@users.noreply.github.com>
2020-08-22 12:22:19 -0700

    Add files via upload

---
+++ P024_02.py
@@ -0,0 +1,23 @@
+# Team Name: copiolis
+# Team Number: P024
+# Problem Number: 02
+import array
+import math
+import re
+import string
+# Class and function definitions:
+# Main program:
+inp=input()
+fin=""
+alph = ["a", "b", "c","d","e","f","g","h","i",
+        "j","k","l","m","n","o","p","q","r","s","t",
+        "u","v","w","x","y","z"]
+
+for i in range(0, 10):
+    old=alph.index(inp[i])
+    nextt=((old+3)%25)
+    if(nextt<4):
+        nextt-=1;
+    fin+= alph[nextt]
+
+print(fin)
