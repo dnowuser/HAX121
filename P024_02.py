dae9e88e569bf0d671961e61320319eaf9f19133
Daniel Suh <62726295+dsuh02@users.noreply.github.com>
2020-08-22 15:09:47 -0700

    Add files via upload

--- P024_02.py
+++ P024_02.py
@@ -13,11 +13,9 @@ alph = ["a", "b", "c","d","e","f","g","h","i",
         "j","k","l","m","n","o","p","q","r","s","t",
         "u","v","w","x","y","z"]
 
-for i in range(0, 10):
+for i in range(0, 26):
     old=alph.index(inp[i])
-    nextt=((old+3)%25)
-    if(nextt<4):
-        nextt-=1;
+    nextt=((old+3)%26)
     fin+= alph[nextt]
 
 print(fin)
