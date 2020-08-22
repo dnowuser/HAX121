5e1c96a68fda7cda6ca7ec202061f521a8714e4b
mindy-kim <39883887+mindy-kim@users.noreply.github.com>
2020-08-22 15:33:29 -0700

    Update P024_02.py

--- P024_02.py
+++ P024_02.py
@@ -13,7 +13,7 @@ alph = ["a", "b", "c","d","e","f","g","h","i",
         "j","k","l","m","n","o","p","q","r","s","t",
         "u","v","w","x","y","z"]
 
-for i in range(0, 10):
+for i in range(len(inp)):
     old=alph.index(inp[i])
     nextt=((old+3)%26)
     fin+= alph[nextt]
